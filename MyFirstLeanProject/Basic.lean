c9ba17ca3d8dd6d1f3a732aa25c2580c22d9617c
muranaka tomonari <tom.ipynb@gmail.com>
2026-03-02 06:09:41 +0900

    natural number gameの復習 at トルクメニスタン空港

--- MyFirstLeanProject/Basic.lean
+++ MyFirstLeanProject/Basic.lean
@@ -1,31 +1,127 @@
-def hello := "world"
+-- 証明の練習
+-- 自然数の基本的な性質を示す
 
-variable (a b c : Nat)
+inductive MyNat where
+  | zero: MyNat
+  | succ: MyNat -> MyNat
 
-theorem test1 : a = a := by
-  rfl -- "Reflexivity"の略。両辺が等しいことを自動で判定します
+def add (m n : MyNat) : MyNat :=
+  match n with
+  | MyNat.zero   => m
+  | MyNat.succ n => MyNat.succ (add m n)
 
-theorem test2 : a + 0 + b = a + b := by
-  rw [Nat.add_zero] -- "a + 0" を "a" に書き換える (Rewrite)
+-- たぶんこれで、中置記法を使えるようにする
+instance : Add MyNat where
+  add := add
 
--- 【問題3】 少し複雑なパズル (交換律の応用)
-theorem test3 : a + b + c = a + c + b := by
-  -- ゴール: a + b + c = a + c + b
-  rw [Nat.add_assoc] -- 結合律 (a+b)+c を a+(b+c) に変形
-  -- ゴール: a + (b + c) = a + c + b
-  rw [Nat.add_comm b c] -- カッコの中の b+c を c+b に変形
-  -- ゴール: a + (c + b) = a + c + b
-  rw [Nat.add_assoc] -- もう一度結合律を使って戻す
-  -- ゴール: a + c + b = a + c + b (両辺が同じになった！)
 
-theorem even_plus_even(m n : Nat) : ∃ k, 2 * m + 2 * n = 2 * k := by
-  exists m + n
-  rw [Nat.mul_add]
+-- 加算に関わる定理
 
-theorem odd_plus_odd (m n : Nat) : ∃ k , (2 * m + 1) + (2 * n + 1) = 2 * k := by
-  exists m + n + 1
-  rw [Nat.add_assoc]
-  rw [Nat.add_comm (2*n) 1]
-  rw [Nat.add_comm]
-  rw [Nat.add_assoc]
+-- ここは自分には魔法のように思える
+-- 後で何故かを確かめる
+theorem add_succ (a b: MyNat): a + MyNat.succ b = MyNat.succ (a + b) := by
+  rfl
+
+-- これは定義より自明
+theorem add_zero (a: MyNat): a + MyNat.zero = a := by
+  rfl
+
+theorem zero_add (a: MyNat): MyNat.zero + a = a := by
+  induction a with
+  | zero => 
+    rfl
+  | succ d hd =>
+    rw [add_succ]
+    rw [hd]
+
+theorem zero_add_zero : MyNat.zero + MyNat.zero = MyNat.zero := by
+  rfl
+
+theorem succ_add (a b : MyNat): MyNat.succ a + b = MyNat.succ (a + b) := by
+  induction b with
+  | zero =>
+    rw [add_zero]
+    rw [add_zero]
+  | succ d hd =>
+    rw [add_succ]
+    rw [hd]
+    rw [add_succ]
+
+theorem add_comm (a b: MyNat): a + b = b + a := by
+  induction a with
+  | zero =>
+    rw [zero_add]
+    rw [add_zero]
+  | succ d hd =>
+    rw [succ_add]
+    rw [add_succ]
+    rw [hd]
+
+theorem add_assoc (a b c: MyNat): a + b + c = a + (c + b) := by
+  induction a with
+  | zero =>
+    rw [zero_add]
+    rw [zero_add]
+    rw [add_comm]
+  | succ k hd =>
+    rw [succ_add]
+    rw [succ_add]
+    rw [succ_add]
+    rw [hd]
+
+-- 掛け算の定義
+
+def mul (m n : MyNat) : MyNat :=
+  match n with
+  | MyNat.zero => MyNat.zero
+  | MyNat.succ n => add (mul m n) m
+
+
+-- たぶんこれで、中置記法を使えるようにする
+instance : Mul MyNat where
+  mul := mul
+
+theorem mul_succ (a b: MyNat): a * MyNat.succ b = a * b + a := by
+  rfl
+
+theorem mul_zero (n: MyNat): n * MyNat.zero = MyNat.zero := by
+  rfl
+
+theorem zero_mul (n: MyNat): MyNat.zero * n = MyNat.zero := by
+  induction n with
+  | zero =>
+    rw [mul_zero]
+  | succ d hd =>
+    rw [mul_succ]
+    rw [hd]
+    rw [zero_add]
+
+theorem mul_one (n: MyNat): n * (MyNat.succ MyNat.zero) = n := by
+  rw [mul_succ]
+  rw [mul_zero]
+  rw [zero_add]
+
+theorem one_mul (n: MyNat): (MyNat.succ MyNat.zero) * n = n := by
+  induction n with
+  | zero =>
+    rw [mul_zero]
+  | succ d hd =>
+    rw [mul_succ]
+    rw [hd]
+    rw [mul_succ]
+    -- succ_eq_add_one: TODO
+
+-- theorem two_mul (n: MyNat): n * (MyNat.succ (MyNat.succ MyNat.zero)) = n + n := by
+--   induction n with
+--   | zero =>
+--     rw [zero_mul]
+--   | succ d hd =>
+--     rw [succ_mul]
+
+theorem succ_mul (a b: MyNat): MyNat.succ a * b = b + a * b := by
+  induction a with
+  | zero =>
+    rw []
+  | succ d hd =>
+
 
