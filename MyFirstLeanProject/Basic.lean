87fc0e2487387fcfaafc9d8004c835a7c8e847c9
muranaka tomonari <tom.ipynb@gmail.com>
2026-03-02 19:33:01 +0900

    掛け算が可換であることの証明まで

--- MyFirstLeanProject/Basic.lean
+++ MyFirstLeanProject/Basic.lean
@@ -14,7 +14,6 @@ def add (m n : MyNat) : MyNat :=
 instance : Add MyNat where
   add := add
 
-
 -- 加算に関わる定理
 
 -- ここは自分には魔法のように思える
@@ -24,6 +23,9 @@ theorem add_succ (a b: MyNat): a + MyNat.succ b = MyNat.succ (a + b) := by
 
 -- これは定義より自明
 theorem add_zero (a: MyNat): a + MyNat.zero = a := by
+  rfl
+
+theorem succ_eq_add_one (n: MyNat): MyNat.succ n = n + MyNat.succ MyNat.zero := by
   rfl
 
 theorem zero_add (a: MyNat): MyNat.zero + a = a := by
@@ -55,6 +57,19 @@ theorem add_comm (a b: MyNat): a + b = b + a := by
   | succ d hd =>
     rw [succ_add]
     rw [add_succ]
+    rw [hd]
+
+theorem add_right_comm (a b c: MyNat): a + b + c = a + c + b := by
+  induction a with
+  | zero =>
+    rw [zero_add]
+    rw [zero_add]
+    rw [add_comm]
+  | succ d hd =>
+    rw [succ_add]
+    rw [succ_add]
+    rw [succ_add]
+    rw [succ_add]
     rw [hd]
 
 theorem add_assoc (a b c: MyNat): a + b + c = a + (c + b) := by
@@ -108,8 +123,8 @@ theorem one_mul (n: MyNat): (MyNat.succ MyNat.zero) * n = n := by
   | succ d hd =>
     rw [mul_succ]
     rw [hd]
-    rw [mul_succ]
-    -- succ_eq_add_one: TODO
+    rw [add_succ]
+    rw [add_zero]
 
 -- theorem two_mul (n: MyNat): n * (MyNat.succ (MyNat.succ MyNat.zero)) = n + n := by
 --   induction n with
@@ -118,10 +133,30 @@ theorem one_mul (n: MyNat): (MyNat.succ MyNat.zero) * n = n := by
 --   | succ d hd =>
 --     rw [succ_mul]
 
-theorem succ_mul (a b: MyNat): MyNat.succ a * b = b + a * b := by
+theorem succ_mul (a b: MyNat): MyNat.succ a * b = a * b + b := by
+  -- (a + 1) *b
+  -- a * b + b
+  induction b with
+  | zero =>
+    rw [mul_zero]
+    rw [mul_zero]
+    rw [add_zero]
+  | succ d hd =>
+    rw [mul_succ]
+    rw [mul_succ]
+    rw [hd]
+    rw [add_succ]
+    rw [add_succ]
+    rw [add_right_comm]
+
+theorem mul_comm (a b : MyNat): a * b = b * a := by
   induction a with
   | zero =>
-    rw []
+    rw [zero_mul]
+    rw [mul_zero]
   | succ d hd =>
+    rw [succ_mul]
+    rw [mul_succ]
+    rw [hd]
 
 
